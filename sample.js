9c84dd536bd0172df2db1733a52e20ff1139788a
jahnavi <jahnavisavara@gmail.com>
2025-09-06 12:37:41 +0530

    commit

--- sample.js
+++ sample.js
@@ -1,1 +1,25 @@
 console.log("this is a sample file")
+let name="jahnavi";
+console.log(name)
+
+num=15;
+console.log(num)
+
+let islogged=false;
+console.log(islogged)
+ 
+let x;
+console.log(x);
+let y=null;
+console.log(y)
+let arr=[1,2,3,4];
+console.log(arr)
+let person={name:"sri",age:20}
+console.log(person.name)
+console.log(typeof person.name)
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof islogged)
+console.log(typeof x)
+console.log(typeof arr)
+console.log(typeof person)
